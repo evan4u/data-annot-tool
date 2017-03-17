07ac42e0e704a26d8b8fc417159782fae89679c2
evan4u <evanbernardez@gmail.com>
2017-03-17 17:21:19 +1100

    Added dynamic button allocation with class names Currently binds tokens to classes

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ $(function() {
 	var html = " My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. Here are some noises animals make: meow, woof, ruff, bark, koo koo, grrrr, moo, ribbit, hsssss, and that is all i can remember. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. Here are some noises animals make: meow, woof, ruff, bark, koo koo, grrrr, moo, ribbit, hsssss, and that is all i can remember. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. Here are some noises animals make: meow, woof, ruf My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. Here are some noises animals make: meow, woof, ruff, bark, koo koo, grrrr, moo, ribbit, hsssss, and that is all i can remember. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. My name is Evan and I like to sleep. I study at Macquarie University where I study engineering. Here are some noises animals make: meow, woof, ruff, bark, koo koo, grrrr, moo, ribbit, hsssss, and that is all i can remember."
 	$( ".context" ).html(html);
 
+	var annotatedData = [];
 
 	var words = [];
 
@@ -35,11 +36,6 @@ $(function() {
   	};
 
 
-
-  	$("input[name='keyword']").on("input", mark);
-  	//$("input[type='checkbox']").on("change", mark);
-
-
 	$(".context").mouseup(function() {
 		var token = removeSpaces(window.getSelection().toString());
 		//var token = window.getSelection().toString();
@@ -48,7 +44,7 @@ $(function() {
 			var r = confirm('Wound you like to highlight '+token);
 			if (r == true) {
 				
-			    words.push(" "+token+" ");
+			    words.push(token);
 			    mark(token);
 			} 
 			
@@ -81,25 +77,6 @@ $(function() {
 	});
 
 
-	// SOMETHING WRONG HERE
-	$(".FAKE").click(function(e){
-		console.log("here");
-        s = window.getSelection();
-        var range = s.getRangeAt(0);
-        var node = s.anchorNode;
-        
-        while(range.toString().indexOf(' ') != 0) {                 
-        	range.setStart(node,(range.startOffset -1));
-        }
-         
-        range.setStart(node, range.startOffset +1);
-        do {
-        	range.setEnd(node,range.endOffset + 1);
-        } while(range.toString().indexOf(' ') == -1 && range.toString().trim() != '');
-        
-        var str = range.toString().trim();
-        alert(str);
-    });
 
 
 	function removeSpaces(str) {
@@ -114,6 +91,47 @@ $(function() {
 
 			return str;
 		}
+	}
+
+	var $input = $("input[name='keyword']");
+  	//$input.on("input", function() {
+  	//	console.log($(this).val());
+  	//});
+
+  	$("#inputClass").keyup(function(event){
+	    if(event.keyCode == 13){
+	        console.log($(this).val());
+	        addClassButton($(this).val());
+	    } 
+	    else if (event.keyCode == 32){
+	    	console.log(annotatedData);
+	    }
+	});
+
+	function addClassButton(type) {
+  		//Create an input type dynamically.   
+  		var element = document.createElement("button");
+  		//Assign different attributes to the element. 
+  		
+  		buttonText = document.createTextNode(type);
+  		
+	  	element.type = type;
+	  	element.value = type; // Really? You want the default value to be the type string?
+	  	
+	  	//element.name = type; // And the name too?
+  		element.onclick = function() { // Note this is a function
+  			addTokensToClass($(this).val())
+  		};
+  		element.appendChild(buttonText);
+  		var foo = document.getElementById("buttonClass");
+  		//Append the element in page (in span).  
+  		foo.appendChild(element);
+  		$('#inputClass').val("");
+	}
+
+	function addTokensToClass(classAnnot) {
+		annotatedData.push({classAnnot: words});
+	
 	}
 
 
